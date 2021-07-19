b7b89d2ee119f832647c1a8a50032fa3d706a6be
vikword <vikword@gmail.com>
2021-07-19 21:34:10 +0300

    Добавьте файлы проекта.

---
+++ Person.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+//Реализовать свой класс Person(ФИО, Дата рождения, Место рождения, Номер паспорта),
+//переопределить в нём методы. GetHashCode и Equals
+
+namespace DexGetHashCode_Equals
+{
+    class Person
+    {
+        public string Name { get; private set; }
+        public DateTime BirthDate { get; private set; }
+        public string BirthPlace { get; private set; }
+        public int PassportNumber { get; private set; }
+
+        public Person(string name, string birthDate, string birthPlace, int passportNumber)
+        {
+            Name = name;
+            if (DateTime.TryParse(birthDate, out _))
+            {
+                BirthDate = DateTime.Parse(birthDate);
+            }
+            BirthPlace = birthPlace;
+            PassportNumber = passportNumber;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, {BirthDate.ToShortDateString()}, {BirthPlace}, {PassportNumber}";
+        }
+
+        public override bool Equals(object obj)
+        {
+            return ToString() == obj.ToString();
+        }
+
+        public bool Equals(Person obj)
+        {
+            if (obj is Person person)
+            {
+                if (GetHashCode() == person.GetHashCode())
+                {
+                    if (this != person)
+                    {
+                        return Name == person.Name
+                            && BirthDate == person.BirthDate
+                            && BirthPlace == person.BirthPlace
+                            && PassportNumber == person.PassportNumber;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode() + BirthDate.GetHashCode() + BirthPlace.GetHashCode() + PassportNumber.GetHashCode();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+//Реализовать упрощенную версию справочника места работ, где ключом является
+//класс Person(ФИО, Дата рождения, Место рождения, Номер паспорта), значением -
+//string(текущее место работы).В качестве результата работы должно быть следующее:
+//Вводим данные о физическом лице: ФИО, Дата рождения, Место рождения, Номер
+//паспорта и нам выдают его текущее место работы, если такой человек существует в
+//нашей базе.
+
+namespace DexGetHashCode_Equals
+{
+    class Program
+    {
+        static void Main()
+        {
+            Person pers1 = new Person("Иванов Иван Иванович", "07.04.1993", "Бендеры", 123456);
+            Person pers2 = new Person("Пертов Сергей Васильевич", "04.09.1992", "Тирасполь", 654321);
+            Person pers3 = new Person("Семенов Иван Николаевич", "05.08.1995", "Рыбница", 234123);
+
+            Dictionary<Person, string> workPlace = new Dictionary<Person, string>
+            {
+                { pers1, "ООО РОГА И КОПЫТА" },
+                { pers2, "ООО КАРАНДАШ" },
+                { pers3, "ООО МЕЧТА" }
+            };
+
+            //Console.WriteLine("Введите данные в формате: Фамилия Имя Отчество, дд.мм.гггг, Город, Номер паспорта");
+            //string search = Console.ReadLine();
+            bool flag = true;
+            foreach (var item in workPlace)
+            {
+                if (item.Key.Equals("Семенов Иван Николаевич, 05.08.1995, Рыбница, 234123"))
+                {
+                    Console.WriteLine(item.Value);
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag)
+            {
+                Console.WriteLine("Объект не существует или некоректно ввели данные.");
+            }
+        }
+    }
+}
